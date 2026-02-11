72270f0d72d5ed0ff0ceebf12a89d2fad7d03264
Arnav Gautam <arnav00782@gmail.com>
2026-02-11 09:55:51 +0530

    trigger redeploy

--- .source/server.ts
+++ .source/server.ts
@@ -1,16 +1,14 @@
 // @ts-nocheck
-import * as __fd_glob_13 from "../content/docs/helpbook/search-basics.mdx?collection=helpbook"
-import * as __fd_glob_12 from "../content/docs/helpbook/integrations.mdx?collection=helpbook"
-import * as __fd_glob_11 from "../content/docs/helpbook/index.mdx?collection=helpbook"
-import * as __fd_glob_10 from "../content/docs/helpbook/getting-started.mdx?collection=helpbook"
-import * as __fd_glob_9 from "../content/docs/developer/test-doc.mdx?collection=developer"
-import * as __fd_glob_8 from "../content/docs/developer/search-api.mdx?collection=developer"
-import * as __fd_glob_7 from "../content/docs/developer/index.mdx?collection=developer"
-import * as __fd_glob_6 from "../content/docs/developer/getting-started.mdx?collection=developer"
-import * as __fd_glob_5 from "../content/docs/developer/api-overview.mdx?collection=developer"
-import * as __fd_glob_4 from "../content/blog/limelight-launch.mdx?collection=blogPosts"
-import * as __fd_glob_3 from "../content/blog/getting-started.mdx?collection=blogPosts"
-import * as __fd_glob_2 from "../content/blog/design-philosophy.mdx?collection=blogPosts"
+import * as __fd_glob_11 from "../content/docs/helpbook/search-basics.mdx?collection=helpbook"
+import * as __fd_glob_10 from "../content/docs/helpbook/integrations.mdx?collection=helpbook"
+import * as __fd_glob_9 from "../content/docs/helpbook/index.mdx?collection=helpbook"
+import * as __fd_glob_8 from "../content/docs/helpbook/getting-started.mdx?collection=helpbook"
+import * as __fd_glob_7 from "../content/docs/developer/test-doc.mdx?collection=developer"
+import * as __fd_glob_6 from "../content/docs/developer/search-api.mdx?collection=developer"
+import * as __fd_glob_5 from "../content/docs/developer/index.mdx?collection=developer"
+import * as __fd_glob_4 from "../content/docs/developer/getting-started.mdx?collection=developer"
+import * as __fd_glob_3 from "../content/docs/developer/api-overview.mdx?collection=developer"
+import * as __fd_glob_2 from "../content/blog/limelight-launch.mdx?collection=blogPosts"
 import * as __fd_glob_1 from "../content/blog/Minimalist-UI.mdx?collection=blogPosts"
 import * as __fd_glob_0 from "../content/blog/AI-J-Curve.mdx?collection=blogPosts"
 import { server } from 'fumadocs-mdx/runtime/server';
@@ -21,8 +19,8 @@ const create = server<typeof Config, import("fumadocs-mdx/runtime/types").Intern
   }
 }>({"doc":{"passthroughs":["extractedReferences"]}});
 
-export const blogPosts = await create.doc("blogPosts", "content/blog", {"AI-J-Curve.mdx": __fd_glob_0, "Minimalist-UI.mdx": __fd_glob_1, "design-philosophy.mdx": __fd_glob_2, "getting-started.mdx": __fd_glob_3, "limelight-launch.mdx": __fd_glob_4, });
+export const blogPosts = await create.doc("blogPosts", "content/blog", {"AI-J-Curve.mdx": __fd_glob_0, "Minimalist-UI.mdx": __fd_glob_1, "limelight-launch.mdx": __fd_glob_2, });
 
-export const developer = await create.docs("developer", "content/docs/developer", {}, {"api-overview.mdx": __fd_glob_5, "getting-started.mdx": __fd_glob_6, "index.mdx": __fd_glob_7, "search-api.mdx": __fd_glob_8, "test-doc.mdx": __fd_glob_9, });
+export const developer = await create.docs("developer", "content/docs/developer", {}, {"api-overview.mdx": __fd_glob_3, "getting-started.mdx": __fd_glob_4, "index.mdx": __fd_glob_5, "search-api.mdx": __fd_glob_6, "test-doc.mdx": __fd_glob_7, });
 
-export const helpbook = await create.docs("helpbook", "content/docs/helpbook", {}, {"getting-started.mdx": __fd_glob_10, "index.mdx": __fd_glob_11, "integrations.mdx": __fd_glob_12, "search-basics.mdx": __fd_glob_13, });
+export const helpbook = await create.docs("helpbook", "content/docs/helpbook", {}, {"getting-started.mdx": __fd_glob_8, "index.mdx": __fd_glob_9, "integrations.mdx": __fd_glob_10, "search-basics.mdx": __fd_glob_11, });

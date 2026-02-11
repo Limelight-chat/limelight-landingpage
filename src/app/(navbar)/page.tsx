b17ffbcb76c321bc59d05666803954aca0567be6
Arnav Gautam <arnav00782@gmail.com>
2026-02-12 00:20:04 +0530

    fixed video alignment

--- src/app/(navbar)/page.tsx
+++ src/app/(navbar)/page.tsx
@@ -145,6 +145,8 @@ export default function Home() {
       <VideoPlayer
         src="https://res.cloudinary.com/dg4g3wdug/video/upload/v1770726853/Analytical-Demo-1_b3ibnx.mp4"
       />
+    </div>,
+    <div key="video3" className="w-[90vw] md:w-[760px] flex items-center justify-center snap-center">
       <VideoPlayer
         src="https://res.cloudinary.com/dg4g3wdug/video/upload/v1770835426/Analytical-Demo-3_oqg4wx.mp4"
       />

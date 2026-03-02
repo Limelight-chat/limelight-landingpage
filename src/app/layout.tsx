6a62a4b030fd9b0d873d0b964e276a36c58e2f3f
Arnav Gautam <arnav00782@gmail.com>
2026-03-03 01:27:40 +0530

    added speed analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Syne } from "next/font/google";
 import { Manrope } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import { ThemeProvider } from "next-themes";
 
@@ -91,6 +92,7 @@ export default function RootLayout({
           {children}
         </ThemeProvider>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
